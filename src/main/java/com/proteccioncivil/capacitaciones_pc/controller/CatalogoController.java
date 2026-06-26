639ef5c767f01ebbe8bb1822e49b04519b43bf56
rodrigoarvisu <rodrigo_arvisu@hotmail.com>
2026-06-26 15:17:33 -0600

    Crear archivos de módulo Catalogos y detallar botones de Capacitaciones al editar registros

---
+++ src/main/java/com/proteccioncivil/capacitaciones_pc/controller/CatalogoController.java
@@ -0,0 +1,4 @@
+package com.proteccioncivil.capacitaciones_pc.controller;
+
+public class CatalogoController {
+}
--- src/main/java/com/proteccioncivil/capacitaciones_pc/entity/Estatus.java
+++ src/main/java/com/proteccioncivil/capacitaciones_pc/entity/Estatus.java
@@ -12,4 +12,6 @@ public class Estatus {
     private Long id;
 
     private String nombre;
+
+    private String descripcion;
 }
